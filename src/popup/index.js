9a771f5c000efdcc2df6932589efd896fe89f200
Rex Liu <rexliu33@gmail.com>
2021-01-16 12:17:19 -0800

    frontend

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-import Input from "@material-ui/core/Input";
 import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import RaisedButton from "@material-ui/core/Button";
 
-
+import ReactFileReader from "react-file-reader";
 
 const pageStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    height: '400px',
+    width: '400px',
+    border: 'solid',
+    borderColor: 'black'
+  },
+  textinput: {
+    width: '250px'
+  },
+  btn: {
+      width: '100px'
+  }
 }));
 
 function PopupPage(props) {
@@ -18,30 +28,43 @@ function PopupPage(props) {
   let [email, setEmail] = React.useState(null);
   let [file, setFile] = React.useState(null);
 
+  let [ics, setIcs] = React.useState(null);
+
   const onChangeHandler = (event) => {
     const { name, value } = event.currentTarget;
 
     if (name === "userName") {
-        setName(value);
-      } else if (name === "userEmail") {
-        setEmail(value);
-      } else if (name === "file") {
-        setFile(value);
-      }
+      setName(value);
+    } else if (name === "userEmail") {
+      setEmail(value);
+    }
   };
 
+  const handleCapture = ({ target }) => {
+    /*const fileReader = new FileReader();
+    fileReader.readAsDataURL(target.files[0]);
+    fileReader.onload = (e) => {
+        setFile(e.target.result)
+    };*/
+  };
+
+  const handleFiles = files => {
+    setFile(files.base64);
+  }
+
   return (
-    <div className={classes.root}>
-        <TextField
-          id="outlined-basic"
-          label="Name"
-          variant="outlined"
-          name="userName"
-          value={name}
-          onChange={(event) => onChangeHandler(event)}
-          className="login__form__textinput"
-          style={{ display: "flex", margin: "1vw auto 0vw auto" }}
-        />
+    <div className="main" className={classes.root}>
+      <TextField
+        id="outlined-basic"
+        label="Name"
+        variant="outlined"
+        name="userName"
+        value={name}
+        onChange={(event) => onChangeHandler(event)}
+        className="main__textinput"
+        className={classes.textinput}
+        style={{ display: "flex", margin: "1vw auto 0vw auto" }}
+      />
       <TextField
         id="outlined-basic"
         label="Email"
@@ -49,22 +72,24 @@ function PopupPage(props) {
         name="userEmail"
         value={email}
         onChange={(event) => onChangeHandler(event)}
-        className="login__form__textinput"
+        className="main__textinput"
+        className={classes.textinput}
         style={{ display: "flex", margin: "1vw auto 0vw auto" }}
       />
-      <Button>
-        <Input
-            id="outlined-basic"
-            label="File"
-            variant="outlined"
-            name="file"
-            value={email}
-            onChange={(event) => onChangeHandler(event)}
-            className="login__form__textinput"
-            style={{ display: "flex", margin: "1vw auto 0vw auto" }}
-        />
-      </Button>
-      
+      {/*<RaisedButton
+        containerElement="label" // <-- Just add me!
+        label="Upload File"
+      >
+        <input type="file" onChange={handleCapture} accept="application/pdf"  />
+      </RaisedButton>*/}
+
+      <ReactFileReader handleFiles={handleFiles} fileTypes={[".pdf"]} base64={true}>
+        <button className={classes.btn}>Upload</button>
+      </ReactFileReader>
+
+      <a href={file} download>
+        Download File
+      </a>
     </div>
   );
 }
