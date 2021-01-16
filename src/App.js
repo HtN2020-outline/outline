c6f73f10b2e9c5d941278dd03f08781e44ec8ee1
Rex Liu <rexliu33@gmail.com>
2021-01-15 20:39:00 -0800

    file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Langar&display=swap" rel="stylesheet"/>
+      <Switch>
+        {/*<Route exact path="/" component={MainPage} />*/}
+      </Switch>
     </div>
+  </BrowserRouter>
   );
 }
 
