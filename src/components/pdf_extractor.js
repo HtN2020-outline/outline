7485fe31c0cc698d25e14715f16d2d885561a7a1
mollyxu <64995721+mollyxu@users.noreply.github.com>
2021-01-16 19:50:10 -0800

    initialized ics

--- src/components/pdf_extractor.js
+++ src/components/pdf_extractor.js
@@ -1,6 +1,22 @@
+const ics = require('ics')
 
 function pdf_extractor(pdf_file) {
-    return an ics file
-}
+    const data = {start: [2021,1,30,9,30], duration:{hours: 2, minutes: 3}, title: "Written Assignment", organizer: {name: 'Admin', email: 'admin@gmail.com'}}
 
+    const event = {
+        start: data.start,
+        duration: data.duration,
+        title: data.title,
+        organizer: data.organizer,
+
+    }
+    ics.createEvent(event, (error, value) => {
+        if (error) {
+          console.log(error)
+          return
+        }
+      
+        console.log(value)
+      })
+}
 export default pdf_extractor;
