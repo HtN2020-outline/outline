dd46dceebf6f99a10936a9ebdaaf98dabc9da5ec
mollyxu <64995721+mollyxu@users.noreply.github.com>
2021-01-16 23:14:21 -0800

    iinitialized ics

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -22,6 +22,24 @@ function get_ics(file) {
       console.log(data.text);      
   });
   return data.text*/
+
+  const data = {start: [2021,1,30,9,30], duration:{hours: 2, minutes: 3}, title: "Written Assignment", organizer: {name: 'Admin', email: 'admin@gmail.com'}}
+
+  const event = {
+      start: data.start,
+      duration: data.duration,
+      title: data.title,
+      organizer: data.organizer,
+
+  }
+  ics.createEvent(event, (error, value) => {
+      if (error) {
+        console.log(error)
+        return
+      }
+    
+      console.log(value)
+    })
   
 }
 
